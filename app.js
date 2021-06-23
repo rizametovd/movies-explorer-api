94d58ea381e2a312fc28a44867cdf6a04c19f2be
David <rizametov@gmail.com>
2021-06-23 11:57:47 +0300

    Настроил cors

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const { routes } = require('./routes');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { rateLimiter } = require('./middlewares/rateLimiter');
 
-const { PORT = 3005 } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 app.use(helmet());
 
